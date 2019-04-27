8b916134655c8c5354113080c82f539664e3860c
hyryo513 <henry.heyi513@gmail.com>
2019-04-26 23:17:15 -0500

    moved images from public to source folder

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -7,18 +7,18 @@ import Footer from "./components/Footer";
 import ClickItem from "./components/ClickItem"
 
 const images = [
-  {id: 1, url: "/images/Jerry_Smith.png"},
-  {id: 2, url: "/images/Rick_Sanchez.png"},
-  {id: 3, url: "/images/Beth_Smith.png"},
-  {id: 4, url: "/images/Diane.jpg"},
-  {id: 5, url: "/images/bird-person.png"},
-  {id: 6, url: "/images/evilmorty.jpeg"},
-  {id: 7, url: "/images/gianthead.png"},
-  {id: 8, url: "/images/picklerick.png"},
-  {id: 9, url: "/images/meeseeks.png"},
-  {id: 10, url: "/images/morty.png"},
-  {id: 11, url: "/images/goldenford.png"},
-  {id: 12, url: "/images/Summer_is_cool.jpg"}
+  {id: 1, url: "./images/Jerry_Smith.png"},
+  {id: 2, url: "./images/Rick_Sanchez.png"},
+  {id: 3, url: "./images/Beth_Smith.png"},
+  {id: 4, url: "./images/Diane.jpg"},
+  {id: 5, url: "./images/bird-person.png"},
+  {id: 6, url: "./images/evilmorty.jpeg"},
+  {id: 7, url: "./images/gianthead.png"},
+  {id: 8, url: "./images/picklerick.png"},
+  {id: 9, url: "./images/meeseeks.png"},
+  {id: 10, url: "./images/morty.png"},
+  {id: 11, url: "./images/goldenford.png"},
+  {id: 12, url: "./images/Summer_is_cool.jpg"}
 ]
 
 class App extends Component {
